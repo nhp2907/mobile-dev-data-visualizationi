1fc376d4de937720aff3945dc9d1c9744a7d40d2
PhatFood <baophatnguyen99@gmail.com>
2021-01-19 20:44:37 +0700

    init charts directory

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {ScrollView} from "react-native-web";
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
@@ -14,7 +13,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
